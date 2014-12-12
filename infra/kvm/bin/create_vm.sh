5c6fd99dc449218dcf8a8ca3453610149eaed79b
David Blaisonneau <david.blaisonneau@orange.com>
2014-12-12 14:29:25 +0100

    add pool choice in create_vm

--- infra/kvm/bin/create_vm.sh
+++ infra/kvm/bin/create_vm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-virsh vol-delete /var/lib/libvirt/images/$NAME.img
-virsh vol-clone --pool default trusty-server-cloudimg-amd64-disk1.img $NAME.img
+
+if [ "$TARGETPOOL" = "ceph" ] && virsh pool-list | grep " ceph " > /dev/null; then
+    TARGETFOLDER='/mnt/cephfs'
+elif [ "$TARGETPOOL" = "default" ] && virsh pool-list | grep " default " > /dev/null; then
+    TARGETFOLDER='/var/lib/libvirt/images'
+else
+    echo "Please provide a valid target pool. Available pools are listed in 'virsh pool-list'"  >&2
+    exit 1
+fi
+
+virsh vol-delete $TARGETFOLDER/$NAME.img
+virsh vol-clone --pool $TARGETPOOL trusty-server-cloudimg-amd64-disk1.img $NAME.img
 virsh create $NAME.xml --console
