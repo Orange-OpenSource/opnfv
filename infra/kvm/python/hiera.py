ba4cea3f40c5319bf95b6b38f18c40fbbb729d0d
David Blaisonneau - Orange <david.blaisonneau@orange.com>
2015-02-24 18:41:35 +0100

    add hiera get method for the python version

---
+++ infra/kvm/python/hiera.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*-coding:utf8 -*
+
+from subprocess import check_output
+import json
+import collections
+
+def hiera(key, ordered=False):
+    obj_pair_hook=collections.OrderedDict if ordered else None
+    args = ['/usr/bin/hiera', key, '-c', '/usr/local/opensteak/infra/hiera.yaml']
+    o = check_output(args,universal_newlines=True)).rstrip()
+
+    if o == 'nil':
+        return None
+    else:
+        try:
+            i = o.replace('=>', ':')
+            return json.loads(i, object_pairs_hook=obj_pair_hook)
+        except:
+            return o
+
+if __name__ == "__main__":
+    print("Ask value:")
+    print(hiera('dns::internal'))
+    print("Ask list:")
+    print(hiera('dns::external'))
+    print("Ask dict:")
+    print(hiera('infra::nodes'))
