dc6e92f79fad86b572fd0beada6c9bde85655b43
David Blaisonneau <david.blaisonneau@orange.com>
2015-03-02 18:49:55 +0100

    Proposal for a class to access and modify opensteak config

---
+++ infra/kvm/python/OpenSteakConf.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*-coding:utf8 -*
+
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+
+class OpensteakConfig:
+    """OpenSteak config class
+    Use this object as a dict
+    """
+    
+    def __init__(self, config_file = "/usr/local/opensteak/infra/config/common.yaml", autosave = False):
+        """Load saved opensteak config.
+        - config_file: the yaml config file to read.
+            default is '/usr/local/opensteak/infra/config/common.yaml'
+        - autosave: save automaticly the config at destroy
+            default is False"""
+        self.config_file = config_file
+        self.autosave = autosave
+        with open(self.config_file, 'r') as stream:
+            self._data = load(stream, Loader=Loader)
+
+    def __getitem__(self, index):
+        """Get an item of the configuration"""
+        return self._data[index]
+
+    def __setitem__(self, index, value):
+        """Set an item of the configuration"""
+        self._data[index] = value
+
+    def dump(self):
+        """Dump the configuration"""
+        return dump(self._data, Dumper=Dumper)
+            
+    def save(self):
+        """Save the configuration to the file"""
+        with open(self.config_file, 'w') as f:
+            f.write(dump(self._data, Dumper=Dumper))
+        
+    def __del__(self):
+        if self.autosave:
+            self.save()
+
+if __name__ == "__main__":
+    c = OpensteakConfig()
+    print(c.dump())
+
