077ffbcfec2151947f7576bc1233b48f3080329d
arnaudmorin <arnaud.morin@gmail.com>
2015-03-18 16:28:20 +0100

    Update compute definition to handle ceph enabled parameter

--- infra/puppet_master/etc/puppet/manifests/nodes/compute.pp
+++ infra/puppet_master/etc/puppet/manifests/nodes/compute.pp
@@ -1,8 +1,13 @@
 node /^compute.*$/ {
+    $ceph_enabled = hiera('ceph::enabled')
+    
     include opensteak::nova-compute
     include opensteak::neutron-compute
-    include opensteak::ceph-mon
-    include opensteak::ceph-mds
-    include opensteak::ceph-osd
-    include opensteak::ceph-client-cinder
+    
+    if str2bool("$ceph_enabled" ){
+        include opensteak::ceph-mon
+        include opensteak::ceph-mds
+        include opensteak::ceph-osd
+        include opensteak::ceph-client-cinder
+    }
 }
