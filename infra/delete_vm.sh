f4db05986c900d761613b1b1dc6e40e38b137e8e
David Blaisonneau <david.blaisonneau@orange.com>
2015-05-13 18:18:18 +0200

    Update delete_vm.sh

--- infra/delete_vm.sh
+++ infra/delete_vm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 NAME=$3
-hammer -u $1 -p $2 host delete --name rabbitmq.infra.opensteak.fr
+hammer -u $1 -p $2 host delete --name $NAME
 echo -e "update delete $NAME a\nupdate delete $NAME txt\nsend" | nsupdate -v -k /etc/bind/rndc.key -l
