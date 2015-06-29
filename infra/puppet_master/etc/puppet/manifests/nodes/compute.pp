cb5e18c2eabcce8d5bc799857beb0906c2be7456
arnaudmorin <arnaud.morin@gmail.com>
2015-06-29 16:49:41 +0200

    remove trailing white spaces

--- infra/puppet_master/etc/puppet/manifests/nodes/compute.pp
+++ infra/puppet_master/etc/puppet/manifests/nodes/compute.pp
@@ -1,9 +1,9 @@
 node /^compute.*$/ {
     $ceph_enabled = hiera('ceph::enabled')
-    
+
     include opensteak::nova-compute
     include opensteak::neutron-compute
-    
+
     if str2bool("$ceph_enabled" ){
         include opensteak::ceph-mon
         include opensteak::ceph-mds
